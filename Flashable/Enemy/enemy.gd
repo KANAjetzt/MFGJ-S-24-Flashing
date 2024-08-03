f93ca3e592d88710e23d50b09462910fad365e68
Kai <kai@kana.jetzt>
2024-08-03 08:49:53 +0200

    refactor: :recycle: enemies get destroyed now / swapped floor out

--- Flashable/Enemy/enemy.gd
+++ Flashable/Enemy/enemy.gd
@@ -1,2 +1,28 @@
 class_name Enemy
 extends Flashable
+
+
+@onready var body_parts: Node3D = %BodyParts
+
+
+func _ready() -> void:
+	flashed.connect(_on_flashed)
+
+
+func unfreeze() -> void:
+	for node in body_parts.get_children():
+		var body_part: RigidBody3D = node
+		body_part.freeze = false;
+		body_part.apply_impulse(
+			Vector3(
+				randf_range(0.0, 1.0),
+				randf_range(0.0, 1.0),
+				randf_range(0.0, 1.0),
+			), 
+			body_part.global_position
+		)
+
+
+func _on_flashed(first_time: bool) -> void:
+	if first_time:
+		unfreeze()
