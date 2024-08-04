944818596db66a40a4bf17e943eab1116f5b20ef
Kai <kai@kana.jetzt>
2024-08-04 11:14:23 +0200

    feat: :sparkles: UI Panel

---
+++ UI/panel_icon_label.gd
@@ -0,0 +1,18 @@
+@tool
+class_name UIPanelIconLabel
+extends PanelContainer
+
+
+@export var label_text := "Example Text" :
+	set(new_value):
+		label_text =  new_value
+		%Label.text = new_value
+@export var icon_texture: Texture :
+	set(new_value):
+		icon_texture = new_value
+		if new_value:
+			%Icon.texture = new_value
+			%Icon.custom_minimum_size = Vector2(30, 30)
+		else:
+			%Icon.texture = null
+			%Icon.custom_minimum_size = Vector2(0, 0)
